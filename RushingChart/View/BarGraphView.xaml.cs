20437bda4fcdb638e28b0451cc090ebdf24d157a
Brendan.Rushing <Brendan.Rushing@peergroup.com>
2021-01-29 04:26:01 -0500

    first commit

---
+++ RushingChart/View/BarGraphView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RushingChart.ViewModel;
+
+namespace RushingChart.View
+{
+    /// <summary>
+    /// Interaction logic for BarGraph.xaml
+    /// </summary>
+    public partial class BarGraph : UserControl
+    {
+
+        /// <summary>
+        /// Create view model.
+        /// </summary>
+        BarGraphViewModel BarGraphViewModel = new BarGraphViewModel();
+
+        public BarGraph()
+        {
+            InitializeComponent();
+
+            // Set the data context so that the view will connect to view model.
+            DataContext = BarGraphViewModel;
+        }
+    }
+}
---
+++ RushingChart/ViewModel/BarGraphViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace RushingChart.ViewModel
+{
+    public class BarGraphViewModel : INotifyPropertyChanged
+    {
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        #region Properties
+
+        public string HelloWorld
+        {
+            get => m_helloWorld;
+            private set
+            {
+                m_helloWorld = value;
+                
+            }
+        }
+
+        #endregion
+
+
+        #region Private Variables
+
+        private string m_helloWorld;
+
+        #endregion
+
+
+
+
+        #region Constructor
+
+        public BarGraphViewModel()
+        {
+            m_helloWorld = "Hy bro";
+        }
+
+        #endregion
+
+    }
+}
